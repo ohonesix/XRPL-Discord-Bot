8b2ee964feadcb66713e49884f02da3d334fc50c
Jacob Pretorius <17687841+jacobpretorius@users.noreply.github.com>
2022-10-17 12:43:24 +0100

    Additional comments

--- src/settings.ts
+++ src/settings.ts
@@ -6,9 +6,19 @@ const SETTINGS = {
     MAX_WALLETS: process.env.MAX_WALLETS || 1,
   },
   DISCORD: {
+    // SERVER_ID
+    // Your Discord server ID, you can this this by right clicking on the server
+    // and choosing 'Copy ID'
     SERVER_ID: process.env.DISCORD_SERVER_ID || 'replace-me',
+    // APP_ID
+    // Your bot Application ID, you get this from the Discord developer portal
     APP_ID: process.env.DISCORD_APP_ID || 'replace-me',
+    // BOT_TOKEN
+    // Your bot token, you get this from the Discord developer portal
     BOT_TOKEN: process.env.DISCORD_BOT_TOKEN || 'replace-me',
+    // ADMIN_IDS
+    // An array of user Discord ID's you want to use the bot's admin commands
+    // e.g. [ '123432423', '8754345', '675675375' ]
     ADMIN_IDS: process.env.DISCORD_ADMIN_IDS || ['replace-me'],
   },
   MONGODB: {
@@ -29,6 +39,9 @@ const SETTINGS = {
     SERVER_URL: process.env.XRPL_SERVER_URL || 'wss://xrplcluster.com/',
     // TOKEN_HASH
     // The token to track. Ideally in the future more than one token will be possible.
+    // e.g. '7850697A7A610000000000000000000000000000' (or if your token is newer and only
+    // has a friendly name use that for example 'GEN'). You can get this from your
+    // setup trustline url
     TOKEN_HASH: process.env.XRPL_TOKEN_HASH || 'replace-me',
   },
   SOLOGENIC: {
@@ -36,18 +49,19 @@ const SETTINGS = {
     // This should not have to change unless Sologenic change their API.
     API_URL: 'https://api.sologenic.org/api/v1/tickers/24h',
     // FORMATTED_TOKEN
+    // This is the same Token Hash used above in the XRPL section, followed by a '+' and
+    // then the issuing wallet address, then add "/XRP" at the end.
     // In the format "TOKENHASH+ISSUERWALLET/XRP" e.g. "7850697A7A610000000000000000000000000000+rUMwLWFzwcD2topAKsg5NBbhq7dtu4gRCU/XRP"
     FORMATTED_TOKEN: process.env.SOLOGENIC_FORMATTED_TOKEN || 'replace-me',
   },
   DISCORD_ROLES: {
     // ROLES_BY_POINTS
-    // Add roles to users based on amount/points holdings
+    // The roles to give users based on amount/points holdings in their linked wallet(s)
     // Format is an array of objects in the format of { Discord Role ID : points required for that role }
     // For example: [ { '123': 1 }, { '111' : 2 }, { '321' : 99 } ]
     ROLES_BY_POINTS: [
-      { '911568176842678302': 1 },
-      { '912293092751724564': 8 },
-      { '911568252692471838': 25 },
+      { 'replace-me': 1 },
+      { 'replace-me': 8 },
     ],
     // ROLES_BY_TOKEN
     // Reserved for future use.
@@ -56,10 +70,10 @@ const SETTINGS = {
   // Using Application Insights is completely optional.
   APPLICATION_INSIGHTS: {
     // ENABLED
-    // Turn logging to Application Insights ON/OFF with 'true' or false.
+    // Turn logging to Application Insights ON/OFF with 'true' or false set as environment variable.
     ENABLED: process.env.AZURE_AI_ENABLED === 'true' ? true : false,
     // KEY
-    // Your Application Insights connection string. Required if 'ENABLED' is 'true'.
+    // Your Application Insights connection string. Required if 'ENABLED' above is 'true'.
     KEY: process.env.AZURE_AI_TOKEN || '',
   },
 };
